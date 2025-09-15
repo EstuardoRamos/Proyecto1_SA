fe6f6ad22441250166c917035308b7cd95760d48
EstuardoRamos <estuardoramosg@gmail.com>
2025-09-15 02:31:40 -0600

    Cors error

--- HotelYRestaurantes/reviews.microservice/src/main/java/com/reviews/microservice/reviewHotel/infrastructure/inputadapters/rest/ReviewHotelControllerAdapter.java
+++ HotelYRestaurantes/reviews.microservice/src/main/java/com/reviews/microservice/reviewHotel/infrastructure/inputadapters/rest/ReviewHotelControllerAdapter.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,7 +30,7 @@ import com.reviews.microservice.reviewHotel.infrastructure.inputadapters.rest.dt
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 
-@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
+//@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
 @RestController
 @RequestMapping("/v1/reviews/hotel")
 //@CrossOrigin(origins = "*")
